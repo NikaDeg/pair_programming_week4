3392bb1b0df80b127aec462d5f20249fc9aba0ba
NikaDeg <garelskaiavv1@gmail.com>
2026-09-07 15:05:32 +0300

    Create user controllers, modify routes

--- week4-be-pp/app.js
+++ week4-be-pp/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const tourRouter = require("./routes/tourRouter.js");
+const userRouter = require("./routes/userRouter.js");
 
 // app.use(tourRouter);
 // Middleware to parse JSON
 app.use(express.json());
 
-app.use('/tours', tourRouter);
+app.use('/api/tours', tourRouter);
+app.use('/api/users', userRouter);
 
 
 const port = 4000;
