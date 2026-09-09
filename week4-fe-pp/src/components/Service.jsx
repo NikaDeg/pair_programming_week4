d3b89d5702ea787b7061a7e59f813d4f256ef559
Aakash-Basnet <aakashb@metropolia.fi>
2026-09-09 15:19:11 +0300

    feat:add removal functionality to Services

--- week4-fe-pp/src/components/Service.jsx
+++ week4-fe-pp/src/components/Service.jsx
@@ -1,15 +1,24 @@
-const Service = ({ icon, title, text, details }) => {
+import Review from './Review';
+
+const Service = ({ id, icon, title, text, details, onRemove }) => {
   return (
-    <article className='service'>
-      <span className='service-icon'>
+    <article className="service">
+      <span className="service-icon">
         <i className={icon}></i>
       </span>
-      <div className='service-info'>
-        <h4 className='service-title'>{title}</h4>
-        <p className='service-text'>{text}</p>
-        {details && <p className='service-details'>{details}</p>}
+      <div className="service-info">
+        <h4 className="service-title">{title}</h4>
+        <p className="service-text">{text}</p>
+        {details && <p className="service-details">{details}</p>}
+        <button
+          className="btn"
+          onClick={() => onRemove(id)}
+          style={{ marginTop: '1rem' }}
+        >
+          Delete me
+        </button>
       </div>
     </article>
-  )
-}
-export default Service
+  );
+};
+export default Service;
--- week4-fe-pp/src/components/Services.jsx
+++ week4-fe-pp/src/components/Services.jsx
@@ -1,22 +1,36 @@
-import { services } from '../data'
-import Title from './Title'
-import Service from './Service'
-import {useState} from 'react'
-
+import { services } from '../data';
+import Title from './Title';
+import Service from './Service';
+import { useState } from 'react';
 
 const Services = () => {
   const [servicesData, setServicesData] = useState(services);
 
-  return (
-    <section className='section services' id='services'>
-      <Title title='our' subTitle='services' />
+  // const handleDeleteItem = (itemId) => {
+  //   const servicesData = setServicesData.filter((item) => item.id !== itemId);
+  //   setItems(updatedItems);
+  // };
 
-      <div className='section-center services-center'>
+  const handleRemoveService = (serviceId) => {
+    setServicesData(servicesData.filter((service) => service.id !== serviceId));
+  };
+
+  return (
+    <section className="section services" id="services">
+      <Title title="our" subTitle="services" />
+
+      <div className="section-center services-center">
         {servicesData.map((service) => {
-          return <Service {...service} key={service.id} />
+          return (
+            <Service
+              {...service}
+              key={service.id}
+              onRemove={handleRemoveService}
+            />
+          );
         })}
       </div>
     </section>
-  )
-}
-export default Services
+  );
+};
+export default Services;
